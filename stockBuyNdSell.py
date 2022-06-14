4ba832e56776c26a42c2165fef45e7f5e11dc285
bharath6401 <103791640+bharath6401@users.noreply.github.com>
2022-06-14 10:01:02 +0530

    Update stockBuyNdSell.py

--- stockBuyNdSell.py
+++ stockBuyNdSell.py
@@ -29,6 +29,18 @@ while(right<len(arr)):
     
 print(maxProfit)
     
+#  method2
+
+# This is a simple problem, if you just step backward in time:
+
+# set the highest sale price to the last value in the array
+# set the highest profit (sale price minus price of stock) to zero.
+# now step backward through the array, and at each point:
+# A. if the stock price is higher than the previously seen highest sale price, set the latter to the former;
+# B. if the highest profit is less than the current highest sale price minus the current stock price, set the former to the latter.
+# And when you've stepped back all the way to the beginning of the array, you'll have, as highest profit, the greatest difference between a stock price, and the highest price seen later than it in the array. So an O(n) solution.
+
+
         
     
         
